eac3f7b670e20f1f3ad743e646949571e7df5d5f
Tóth Fanni <tothjanostamas@gmail.com>
2024-10-10 01:02:18 +0200

    LoginPage.jsx created/Bejelentkezesi oldal létrehozása,alap funkciók

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import loginPageCSS from './../loginPage/LoginPage.module.css';
 
 function LoginPage() {
     return (
-        <div>Teszt</div>
+        <div className={loginPageCSS.wrapper}>
+            <div className="form-box login">
+                <form action="">
+                    <h1>Bejelentkezés</h1>
+                    <div className={loginPageCSS.inputBox}>
+                        <input type="text" placeholder="Felhasználónév"  required />
+                    </div>
+                    <div className={loginPageCSS.inputBox}>
+                        <input type="password" placeholder="Jelszó" required />
+                    </div>
+                    <div className={loginPageCSS.forgotPass}>
+
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 

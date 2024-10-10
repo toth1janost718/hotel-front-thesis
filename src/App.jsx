0f0f84f056a6cdeeec4a0299cfd53b6110463b83
toth1janost718 <toth.janos.tamas@gmail.com>
2024-10-10 21:53:04 +0200

    created global.css/Az összeakadáso miatt global.css létrehozása

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
+import './global.css'
 import {BrowserRouter as  Router} from "react-router-dom";
 import AppRoutes from './AppRoutes.jsx'
+
 
 function App() {
 
--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import loginPageCSS from './../loginPage/LoginPage.module.css';
+import {FaUser,FaLock} from 'react-icons/fa';
+import {useEffect} from "react";
+
 
 function LoginPage() {
+    useEffect(() => {
+        document.body.classList.add('login-body');
+
+        return () => {
+            document.body.classList.remove('login-body');
+        };
+    }, []);
     return (
         <div className={loginPageCSS.wrapper}>
             <div className="form-box login">
@@ -9,13 +19,17 @@ function LoginPage() {
                     <h1>Bejelentkezés</h1>
                     <div className={loginPageCSS.inputBox}>
                         <input type="text" placeholder="Felhasználónév"  required />
+                        <FaUser className={loginPageCSS.icon}/>
                     </div>
                     <div className={loginPageCSS.inputBox}>
                         <input type="password" placeholder="Jelszó" required />
+                        <FaLock className={loginPageCSS.icon}/>
                     </div>
                     <div className={loginPageCSS.forgotPass}>
-
+                        <a href="">Elfelejtette a jelszavát?</a>
                     </div>
+                    <button type="submit" className={loginPageCSS.loginButton}>Bejelentkezés</button>
+
                 </form>
             </div>
         </div>

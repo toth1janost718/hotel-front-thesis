207238f56c1e414b08bea17a973ea574b0f99fc9
toth1janost718 <toth.janos.tamas@gmail.com>
2024-12-13 15:48:49 +0100

    Reset password method

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import loginPageCSS from './LoginPage.module.css';
-import { FaUser, FaLock, FaEnvelope } from 'react-icons/fa';
+import { FaUser, FaLock } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import config from "../../../config.js";
@@ -19,6 +19,8 @@ function LoginPage() {
     const [action, setAction] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
 
@@ -72,6 +74,43 @@ function LoginPage() {
         }
     };
 
+    const handlePasswordReset = async (e) => {
+        e.preventDefault();
+
+        if (!username || !newPassword || !confirmPassword) {
+            alert("Kérjük, töltse ki az összes mezőt!");
+            return;
+        }
+
+        if (newPassword !== confirmPassword) {
+            alert("A jelszavak nem egyeznek!");
+            return;
+        }
+
+        try {
+            const response = await fetch(`${config.hrApiBaseUrl}/api/UserLogin/update-password`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    UserName: username,
+                    NewPassword: newPassword
+                })
+            });
+
+            if (response.ok) {
+                alert("A jelszó sikeresen frissítve.");
+                setAction('');
+            } else {
+                const data = await response.json();
+                alert(data.message || "Hiba történt a jelszó frissítése során.");
+            }
+        } catch (error) {
+            alert("Nem sikerült csatlakozni a szerverhez.");
+        }
+    };
+
 
     return (
         <div className={`${loginPageCSS.wrapper} ${action === 'active' ? loginPageCSS.active : ''}`}>
@@ -108,18 +147,39 @@ function LoginPage() {
 
             <div
                 className={`${loginPageCSS.formBox} ${loginPageCSS.register} ${action === 'active' ? loginPageCSS.activeRegister : ''}`}>
-                <form action="">
-                    <h1>Elfelejtett jelszó</h1>
+                <form onSubmit={handlePasswordReset}>
+                    <h1>Jelszó beállítása</h1>
                     <div className={loginPageCSS.inputBox}>
-                        <input type="text" placeholder='Felhasználónév' required />
-                        <FaUser className={loginPageCSS.icon} />
+                        <input
+                            type="text"
+                            placeholder='Felhasználónév'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                        <FaUser className={loginPageCSS.icon}/>
+                    </div>
+                        <div className={loginPageCSS.inputBox}>
+                        <input
+                            type="password"
+                            placeholder='Új jelszó'
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            required
+                        />
+                        <FaLock className={loginPageCSS.icon}/>
                     </div>
                     <div className={loginPageCSS.inputBox}>
-                        <input type="email" placeholder='E-mail' required />
-                        <FaEnvelope className={loginPageCSS.icon} />
+                        <input
+                            type="password"
+                            placeholder='Jelszó megerősítése'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required/>
+                        <FaLock className={loginPageCSS.icon}/>
                     </div>
 
-                    <button type='submit'>Jelszó küldése</button>
+                    <button type='submit'>Mentés</button>
                     <div className={loginPageCSS.registerLink}>
                         <p>Be szeretne jelentkezni? <a href="#" onClick={loginLink}> Belépés </a></p>
                     </div>
